cc96187384fb6e4b993d659a7c1434e754e0b4ba
Ziang_Wei <ziang.wei@tum.de>
2025-10-25 23:53:49 +0800

    update

--- local_configs/NYUDepthv2/DFormerv2_S.py
+++ local_configs/NYUDepthv2/DFormerv2_S.py
@@ -26,7 +26,7 @@ C.text_encoder_name = None
 # 标签与描述数据
 C.label_txt_path = "datasets/NYUDepthv2/nyu40_labels.txt"
 C.caption_json_path = "datasets/NYUDepthv2/generated_rgb_descriptions_internvl3.json"
-C.image_labels_json_path = "datasets/NYUDepthv2/topk_labels_text_internvl3.json"
+C.image_labels_json_path = "datasets/NYUDepthv2/topk_labels2_text_internvl3.json"
 # C.image_labels_json_path = "datasets/NYUDepthv2/top5_labels_per_image.json"
 # 模板与数量（对“类名→多模板短句”的扩写）
 C.text_template_set = "clip"           # clip / none
